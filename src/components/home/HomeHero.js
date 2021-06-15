2b968f0b758ef97f0629b37e66ecf45050133950
fredThem <fdesmarais.them@gmail.com>
2021-06-14 21:06:32 -0400

    fixed parralax and contrast

--- src/components/home/HomeHero.js
+++ src/components/home/HomeHero.js
@@ -11,7 +11,7 @@ import './parallaxHero.css';
 // import { makeStyles } from "@material-ui/core/styles";
 // import Paper from "@material-ui/core/Paper";
 // import Grid from "@material-ui/core/Grid";
-import { Typography, Grid, Container } from '@material-ui/core';
+import { Box, Typography, Grid, Container } from '@material-ui/core';
 import '@fontsource/fira-code'; // Defaults to weight 400.
 // import { Typography } from '@material-ui/core/Typography';
 // import { shadows } from '@material-ui/system';
@@ -19,7 +19,10 @@ import '@fontsource/fira-code'; // Defaults to weight 400.
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    marginTop: '-25%',
+    // sm: {
+    //   marginTop: '-25%',
+    // }
+    // marginTop: '-25%',
   },
   paper: {
     padding: theme.spacing(2),
@@ -31,8 +34,10 @@ const useStyles = makeStyles((theme) => ({
     // so a smaller fontsize may be appropriate.
     fontFamily: 'Fira Code',
     fontWeight: '700',
+    // fontSize: '1.2 em',
     color: 'white',
-    textShadow: '1px 1px 1px rgba(0,0,180,0.8), 1px 0px 3px rgba(0,0,100,0.3)',
+    // textShadow: '2px 1px 0px rgba(0,0,180,0.7), 2px 3px 0px rgba(0,0,180,0.7)',
+    textShadow: '-1px -1px 0 #2c65ad, 1px -1px 0 #2c65ad, -1px 1px 0 #2c65ad, 1px 1px 0 #2c65ad',
     // marginBottom: "10px",
   },
 }));
@@ -68,7 +73,7 @@ const HomeHero = (/* props */) => {
             <ParallaxBanner
               className={ParallaxBannerStyles.bannerBg}
               layers={layers}
-              style={{ height: '80vh', maxHeight: '720px' }}
+              style={{ height: '640px' }}
             >
               <div
                 className={ParallaxBannerStyles.parallaxChildren}
@@ -84,84 +89,125 @@ const HomeHero = (/* props */) => {
                     </Grid> */}
                       <Grid
                         item
-                        xs={12}
+                        // xs={12}
                         sm={8}
                         md={8}
                       >
-                        <Typography
-            // variant="h6"
-            // component="h2"
-                          className={classes.typography}
+                        <Box
+                          sx={{ backgroundColor: 'rgba(0,0,50, 0)' }}
+                          p={2}
                         >
-                          #
-                          {' '}
-                          Freshly certified 🎉
-                          {' '}
-                        </Typography>
-                        <Typography
-                          variant="h5"
-            // component="h2"
-                          className={classes.typography}
-                        >
-                          {'`<FullStackWebDeveloper/>`'}
-                        </Typography>
-                        <Typography
-                          className={classes.typography}
-                          style={{ margin: '16px' }}
-                        >
-                          {'>'}
-                          {' '}
-                          With a technical background in web & graphic design.
-                          {' '}
-                        </Typography>
-                        <Typography
-            // variant="h6"
-            // component="h2"
-                          className={classes.typography}
-                        >
-                          {`I_love: [ 
-                Coding with a clear and practical style 👾,
-                Solving problems🚀,
-                Being outdoors ⛰‍,
-                Hard rock 👹]`}
-                        </Typography>
+                          <Typography
+                            variant="h5"
+                                      // component="h2"
+                            className={classes.typography}
+                          >
+                            #
+                            {' '}
+                            Freshly certified 🎉
+                            {' '}
+                          </Typography>
+                          <Typography
+                            variant="h4"
+                                      // component="h2"
+                            mb={3}
+                            className={classes.typography}
+                          >
+                            {'`<FullStackWebDeveloper/>`'}
+                          </Typography>
+                          <Typography
+                            className={classes.typography}
+                            // style={{ margin: '16px' }}
+                            variant="subtitle1"
+                            my={3}
+                          >
+                            {'>'}
+                            {' '}
+                            With a technical background in web & graphic design.
+                            {' '}
+                          </Typography>
+                          <Typography
+                            variant="body1"
+                                      // component="h2"
+                            className={classes.typography}
+                          >
+                            I_LOVE: [
+                            Coding with a clear and practical style &#x1F47E;,
+                            <Box
+                              component="span"
+                              pl="2em"
+                              display="block"
+                            >
+                              Solving problems &#x1F680;,
+                            </Box>
+                            <Box
+                              component="span"
+                              pl="2em"
+                              display="block"
+                            >
+                              Being outdoors &#x26F0;&#x200D;,
+                            </Box>
+                            <Box
+                              component="span"
+                              pl="2em"
+                              display="block"
+                            >
+                              Hard rock &#x1F479;]
+                            </Box>
+                          </Typography>
+                        </Box>
                       </Grid>
                       <Grid
+                        // backgroundColor="background.default"
                         item
-                        xs={12}
+                        // xs={12}
                         sm={4}
                         md={4}
                       >
-                        <Typography
-                          variant="h6"
-            // component="h2"
-                          className={classes.typography}
+                        <Box
+                          sx={{ backgroundColor: 'rgba(0,0,50, 0.0)' }}
+                          p={2}
                         >
-                          Hello world, I’m Fred
-                          {'\u2009'}
-                          🖐
-                        </Typography>
-                        <Typography className={classes.typography}>
-                          {'>'}
-                          {' '}
-                          BASED IN MTL
-                        </Typography>
-                        <Typography className={classes.typography}>
-                          {'>'}
-                          {' '}
-                          EN/FR BILINGUAL
-                        </Typography>
+
+                          <Typography
+                            variant="h6"
+                                    // component="h2"
+                            className={classes.typography}
+                          >
+                            Hello world, I’m Fred
+                            {'\u2009'}
+                            🖐
+                          </Typography>
+                          <Typography className={classes.typography}>
+                            {'>'}
+                            {' '}
+                            BASED IN MTL
+                          </Typography>
+                          <Typography className={classes.typography}>
+                            {'>'}
+                            {' '}
+                            EN/FR BILINGUAL
+                          </Typography>
+                        </Box>
                       </Grid>
                     </Grid>
                   </div>
                 </Container>
                 <img
+                  className="heroBanner"
                   src="https://res.cloudinary.com/hhdcknvdc/image/upload/v1623709331/landingPage/mountain-foregroundNme__xcbhjo.png"
                   // onClick={() => console.log('onClick')}
                   // aspectRatio={(16 / 9)}
-                  style={{ width: '100%', height: 'auto', marginTop: '-80%' }}
+                  // style={{ width: '100%', height: 'auto', marginTop: '-80%' }}
                   // disableSpinner
                   alt="me"
+                  // sx={{
+                  //   sm: {
+                  //     objectFit: 'cover',
+                  //     objectPosition: '80% top',
+                  //     marginTop: '-80%',
+                  //     height: '40%'
+                  //   } }}
                 />
               </div>
             </ParallaxBanner>
