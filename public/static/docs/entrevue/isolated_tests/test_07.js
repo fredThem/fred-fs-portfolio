34d2d7524902a5cbb6e84d47387414b08ad2da1b
fredThem <fdesmarais.them@gmail.com>
2021-07-02 08:58:01 -0400

    test_07

--- public/static/docs/entrevue/isolated_tests/test_07.js
+++ public/static/docs/entrevue/isolated_tests/test_07.js
@@ -1,60 +1,3 @@
----
-title: Absolunet Test
----
-
-# Test Front End
-
-Entrevue v2.0.2
-
-## 7. Améliorer ce code JS (tout le JS peut être changé)
-
-
-### ✍️ [Codepen view](https://codepen.io/them-sayer/pen/PomqREj)  
-
-
-[Contexte HTML] 
-
-
-```html
-<div id="mine"></div>
-<div class="welcome" data-hello="qwertyuiop"></div>
-<div data-dolor="12"></div>
-```
-
-
-### Before
-
-```javascript
-const test = hello_world('welcome'); // 'helloWorld' was used before it was defined.
-console.log(test + ' [test]');
-
-function hello_world(foo = 'ipsum', bar) { // 'bar' is defined but never used.
-    const $foo = $('.' + foo); //'$' is not defined.
-    return foo + ': ' + $foo.data('hello');
-}
-
-const loremIpsum = (dolor) => {
-    dolor = dolor || 'dolor';
-    let test = '123456';
-    let val = $('[data-'+dolor+']').data(dolor);
-    return `<p>Result [${test}]: ${test * val}</p>`;
-};
-
-const myDiv = $('#mine');
-myDiv.html(loremIpsum());
-myDiv.css("background-color", "cyan");
-myDiv.on('click', () => {
-    $(this).find('p').css('color', 'red');
-});
-
-let abc = { x:'1', y:'2', z:'3' };
-const x = abc.x;
-const y = abc.y;
-console.log(x == 1 && y >= 2 ? 'yeah' : 'boo');
-```
-
-### after 
-```javascript
 function helloWorld(foo) {
   console.log(`input is: ${foo}`);
   const cardGreetings = document.querySelector('.welcome');
@@ -69,6 +12,8 @@ function helloWorld(foo) {
 console.log(helloWorld('welcome'));
 helloWorld('welcome');
 helloWorld(1);
+
+// console.log(hellowWorld('welcome'));
 
 function loremIpsum(dolor) {
   const ipsummary = ('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.').split(' ');
@@ -88,4 +33,3 @@ myDiv.addEventListener('click', () => {
   loremIpsum(12);
   myDiv.style.color = 'red';
 });
-```
